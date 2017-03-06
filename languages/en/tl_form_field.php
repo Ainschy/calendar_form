5ccbae4dceffb523c83d386f47a1cc07e266735a
Oliver Willmes <info@oliverwillmes.de>
2017-03-06 11:10:21 +0100

    Translate to english

--- languages/en/tl_form_field.php
+++ languages/en/tl_form_field.php
@@ -11,54 +11,55 @@
  * @copyright Oliver Willmes 2017
  */
 
-$GLOBALS['TL_LANG']['FFL']['formcalendar'] = ['Kalender', 'Stellt einen Kalenderansicht zur Auswahl von Terminen bereit.'];
+$GLOBALS['TL_LANG']['FFL']['formcalendar'] = ['Calendar', 'Provides a calendar view to select appointments.'];
 
 /**
  * Fields
  */
 
-$GLOBALS['TL_LANG']['tl_form_field']['calender_legend'] = 'Kalender Einstellungen';
-$GLOBALS['TL_LANG']['tl_form_field']['calenderpro_legend'] = 'Abhängigkeiten und Buchungen';
-$GLOBALS['TL_LANG']['tl_form_field']['selectCalendar'] = ['Kalender', 'Wählen Sie einen oder mehrere Kalender zur Buchung aus.'];
-$GLOBALS['TL_LANG']['tl_form_field']['cal_startDay'] = ['Wochenstart', 'Wählen Sie den Tag an dem die Wochen beginnen soll.'];
+$GLOBALS['TL_LANG']['tl_form_field']['calender_legend'] = 'Calendar settings';
+$GLOBALS['TL_LANG']['tl_form_field']['calenderpro_legend'] = 'Dependencies and bookings';
+$GLOBALS['TL_LANG']['tl_form_field']['selectCalendar'] = ['Calendar', 'Select one or more calendars to book.'];
+$GLOBALS['TL_LANG']['tl_form_field']['cal_startDay'] = ['Start day of the week', 'Select the day on which the week starts.'];
 
-$GLOBALS['TL_LANG']['tl_form_field']['calForm'] = ['Kalenderdarstellung', 'Wählen die passende Darstellung aus.'];
-$GLOBALS['TL_LANG']['tl_form_field']['calForms']['month'] = 'Monat';
-$GLOBALS['TL_LANG']['tl_form_field']['calForms']['week'] = 'Woche';
-$GLOBALS['TL_LANG']['tl_form_field']['getCalender']['default'] = 'ohne Kalender';
-$GLOBALS['TL_LANG']['tl_form_field']['calSetReservation']['1'] = 'eintragen ohne Aktivierung';
-$GLOBALS['TL_LANG']['tl_form_field']['calSetReservation']['2'] = 'eintragen mit Aktivierung';
+$GLOBALS['TL_LANG']['tl_form_field']['calForm'] = ['Calendar display', 'Select the appropriate display.'];
+$GLOBALS['TL_LANG']['tl_form_field']['calForms']['month'] = 'Month';
+$GLOBALS['TL_LANG']['tl_form_field']['calForms']['week'] = 'Week';
+$GLOBALS['TL_LANG']['tl_form_field']['getCalender']['default'] = 'no calendar';
+$GLOBALS['TL_LANG']['tl_form_field']['calSetReservation']['1'] = 'without activation';
+$GLOBALS['TL_LANG']['tl_form_field']['calSetReservation']['2'] = 'with activation';
 
-$GLOBALS['TL_LANG']['tl_form_field']['calChoise_label'] = ['Einschränkunen'];
-$GLOBALS['TL_LANG']['tl_form_field']['calChoise']['selection'] = 'Auswahl - nur definierte Termine sind auswählbar.';
-$GLOBALS['TL_LANG']['tl_form_field']['calChoise']['exception'] = 'Ausnahmen - Ausnahmen sind nicht buchbar, z.B. Urlaub/Feiertage';
+$GLOBALS['TL_LANG']['tl_form_field']['calChoise_label'] = ['limitations'];
+$GLOBALS['TL_LANG']['tl_form_field']['calChoise']['selection'] = 'Selection - only defined appointments are selectable.';
+$GLOBALS['TL_LANG']['tl_form_field']['calChoise']['exception'] = 'Exceptions - Exceptions are not bookable, e.g. Holidays / Holidays';
 
-$GLOBALS['TL_LANG']['tl_form_field']['calLogicMonth'] = ['Max. Auswahl', 'Schränkt die maximal buchbaren Tage ein. 0=keine Einschränkung'];
-$GLOBALS['TL_LANG']['tl_form_field']['calLogicWeek'] = ['Auswahlregel', 'Schränke die Auwahl ein.'];
-$GLOBALS['TL_LANG']['tl_form_field']['calLogics']['onlyOne'] = '1 Termin';
-$GLOBALS['TL_LANG']['tl_form_field']['calLogics']['onePerDay'] = '1 Termin pro Tag';
-$GLOBALS['TL_LANG']['tl_form_field']['calLogics']['multiPerDay'] = 'x Termine pro Tag';
+$GLOBALS['TL_LANG']['tl_form_field']['calLogicMonth'] = ['Max. Selection', 'Restricts the maximum bookable days. 0 = no restriction'];
+$GLOBALS['TL_LANG']['tl_form_field']['calLogicWeek'] = ['Selection rule', 'Enter the selection.'];
+$GLOBALS['TL_LANG']['tl_form_field']['calLogics']['onlyOne'] = 'only one appointment';
+$GLOBALS['TL_LANG']['tl_form_field']['calLogics']['onePerDay'] = 'only one appointment per day';
+$GLOBALS['TL_LANG']['tl_form_field']['calLogics']['multiPerDay'] = 'multi appointment per day';
 
-$GLOBALS['TL_LANG']['tl_form_field']['calRange'] = ['Zeitraum', 'Start- und Enddatum bilden eine Zeitraum.'];
+$GLOBALS['TL_LANG']['tl_form_field']['calRange'] = ['Period of time', 'Start and end dates are a period.'];
 
-$GLOBALS['TL_LANG']['tl_form_field']['available'] = ['Auswahl definieren', 'Folgende Formate werden unterstützt: 08:00, 08:00/60, 08:00-09:00'];
-$GLOBALS['TL_LANG']['tl_form_field']['exceptions'] = ['Auswahl / Ausnahmen (Datum von - bis)', 'Auswahl / Ausnahmen definieren buchbare oder geblockte Zeiten'];
-$GLOBALS['TL_LANG']['tl_form_field']['startdate'] = ['Beginn', ''];
-$GLOBALS['TL_LANG']['tl_form_field']['enddate'] = ['Ende', ''];
+$GLOBALS['TL_LANG']['tl_form_field']['available'] = ['Define selection', 'The following formats are supported: 08:00, 08:00/60, 08:00-09:00'];
+$GLOBALS['TL_LANG']['tl_form_field']['exceptions'] = ['Selection / Exceptions (Date from - to)', 'Selection / exceptions define bookable or blocked times.'];
+$GLOBALS['TL_LANG']['tl_form_field']['startdate'] = ['Start', ''];
+$GLOBALS['TL_LANG']['tl_form_field']['enddate'] = ['End', ''];
 
-$GLOBALS['TL_LANG']['tl_form_field']['calOutput'] = ['Datumsformat in der Email', 'Der Datumsformat-String wird mit der PHP-Funktion date() geparst.'];
-$GLOBALS['TL_LANG']['tl_form_field']['calOutputTime'] = ['Zeitformat in der Email', 'Der Zeitformat-String wird mit der PHP-Funktion date() geparst.'];
+$GLOBALS['TL_LANG']['tl_form_field']['calOutput'] = ['Date format in the email', 'The date format string is parsed using the PHP function date ().'];
+$GLOBALS['TL_LANG']['tl_form_field']['calOutputTime'] = ['Time format in the email', 'The time format string is parsed using the PHP function date ().'];
 
-$GLOBALS['TL_LANG']['tl_form_field']['monday'] = ['Montags', ''];
-$GLOBALS['TL_LANG']['tl_form_field']['tuesday'] = ['Dienstags', ''];
-$GLOBALS['TL_LANG']['tl_form_field']['wednesday'] = ['Mittwochs', ''];
-$GLOBALS['TL_LANG']['tl_form_field']['thursday'] = ['Donnerstags', ''];
-$GLOBALS['TL_LANG']['tl_form_field']['friday'] = ['Freitags', ''];
-$GLOBALS['TL_LANG']['tl_form_field']['saturday'] = ['Samstags', ''];
-$GLOBALS['TL_LANG']['tl_form_field']['sunday'] = ['Sonntag', ''];
+$GLOBALS['TL_LANG']['tl_form_field']['monday'] = ['Monday', ''];
+$GLOBALS['TL_LANG']['tl_form_field']['tuesday'] = ['Tuesday', ''];
+$GLOBALS['TL_LANG']['tl_form_field']['wednesday'] = ['Wednesday', ''];
+$GLOBALS['TL_LANG']['tl_form_field']['thursday'] = ['Thursday', ''];
+$GLOBALS['TL_LANG']['tl_form_field']['friday'] = ['Friday', ''];
+$GLOBALS['TL_LANG']['tl_form_field']['saturday'] = ['Saturday', ''];
+$GLOBALS['TL_LANG']['tl_form_field']['sunday'] = ['Sunday', ''];
 
 // Kopfbereich der Tabelle
-$GLOBALS['TL_LANG']['CAL_FORM']['elements']['removeAll'] = 'Auswahl aufheben';
+$GLOBALS['TL_LANG']['CAL_FORM']['elements']['removeAll'] = 'Deselect
+';
 $GLOBALS['TL_LANG']['CAL_FORM']['elements']['next'] = '<a data-id="<%next.id%>" class="next"><%next.label%> &gt;</a>';
 $GLOBALS['TL_LANG']['CAL_FORM']['elements']['prev'] = '<a data-id="<%prev.id%>" class="prev">&lt; <%prev.label%></a>';
 $GLOBALS['TL_LANG']['CAL_FORM']['elements']['current'] = '<select class="current"><%#current%><%{options}%><%/current%></select>';
@@ -78,5 +79,5 @@ $GLOBALS['TL_LANG']['CAL_FORM']['elements']['day_option_label_end'] = '%s - %s';
 $GLOBALS['TL_LANG']['CAL_FORM']['elements']['month_format_prev-next'] = 'M Y';
 $GLOBALS['TL_LANG']['CAL_FORM']['elements']['month_format_current'] = 'F Y';
 $GLOBALS['TL_LANG']['CAL_FORM']['elements']['week_format_prev-next'] = 'W';
-$GLOBALS['TL_LANG']['CAL_FORM']['elements']['week_format_current'] = 'KW %s (%s - %s)';
+$GLOBALS['TL_LANG']['CAL_FORM']['elements']['week_format_current'] = 'Week %s (%s - %s)';
 $GLOBALS['TL_LANG']['CAL_FORM']['elements']['week_format_current_option'] = 'W';
